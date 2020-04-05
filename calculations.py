14fb3b0b230d00bbf64b045fa80ab67fe5a327f9
mmiah00 <mmiah00@stuy.edu>
2020-04-05 03:13:16 +0100

    working on generating a board, got bugs to fix

--- calculations.py
+++ calculations.py
@@ -1,33 +1,47 @@
 #! /usr/bin/python
+import random
 
-cliques = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-possible_games = 0
-data = dict ()
-for i in range (9):
-    data[i] = " "
-moves = ""
+class Board:
+    cliques = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-def check_win ():
-    for clique in cliques:
-        if (data[clique[0]] == data[clique[1]]) and (data[clique[1]] == data[clique[2]]) and (data[clique[0]] == data[clique[2]]):
+    def __init__ (self):
+        self.data = dict ()
+        for i in range (9):
+            self.data[i] = " "
+        self.moves = ""
+
+    def check_win (self):
+        for clique in self.cliques:
+            if (self.data[clique[0]] != " ") and (self.data[clique[1]] != " ") and (self.data[clique[2]] != " "):
+                if (self.data[clique[0]] == self.data[clique[1]]) and (self.data[clique[1]] == self.data[clique[2]]) and (self.data[clique[0]] == self.data[clique[2]]):
+                    return True
+        return False
+
+    def print_board (self):
+        ans = ''
+        for position in self.data.keys ():
+            if position == 0 or position == 1 or position == 3 or position == 4 or position == 6 or position == 7:
+                ans += " " + self.data[position] + " |"
+            else:
+                if position != 8:
+                    ans += " " + self.data[position] + "\n-----------\n"
+        print (ans)
+
+    def make_board (self, turn, num_moves):
+        if self.check_win () or num_moves == 9:
             return True
-    return False
-
-def print_board ():
-    ans = ''
-    for position in data.keys ():
-        if position == 0 or position == 1 or position == 3 or position == 4 or position == 6 or position == 7:
-            ans += " " + data[position] + " |"
         else:
-            if position != 8:
-                ans += " " + data[position] + "\n-----------\n"
-    print (ans)
+            spot = random.randint (0,8)
+            if self.data[spot] != " ": #filled
+                self.make_board (turn, num_moves) #try again
+            else:
+                if turn == 0:
+                    self.data[spot] = "x"
+                    self.make_board (1, num_moves + 1)
+                else:
+                    self.data[spot] = "o"
+                    self.make_board (0, num_moves + 1)
 
-data[0] = "x"
-data[1] = "x"
-data[3] = "x"
-data[2] = "o"
-data[4] = "o"
-data[6] = "o"
-
-print_board ()
+a = Board()
+a.make_board (0, 1)
+a.print_board()
