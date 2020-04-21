76faac1a607518fed25ce3701162903684bf3181
mmiah00 <mmiah00@stuy.edu>
2020-04-21 21:43:30 +0100

    adds up to the right number but why are there so many nones

--- ttt2.py
+++ ttt2.py
@@ -1,4 +1,3 @@
-
 #! /usr/bin/python3
 import sys
 import time
@@ -28,21 +27,6 @@ class BoardNode:
     def print_me(self):
         print ('layout:',self.layout, 'endState:',self.endState)
         print ('children:',self.children)
-
-    def print_board (self):
-        ans = ''
-        for position in range (9):
-            turn = self.layout[position]
-            if turn == '_':
-                turn = " "
-            if position == 0 or position == 1 or position == 3 or position == 4 or position == 6 or position == 7:
-                ans += " " + turn + " |"
-            else:
-                if position != 8:
-                    ans += " " + turn + "\n-----------\n"
-                else:
-                    ans += " " + turn
-        print (ans)
 
     def checkwin (self):
         for clique in Wins:
@@ -92,33 +76,42 @@ def CreateAllBoards (layout, parent): #parent is a BoardNode
                         temp = b.layout
                         b.layout = replace ('o', position, temp)
                         CreateAllBoards (b.layout, b)
-                    AllBoards[b.layout] = b
-            if b.endState == 'd':
-                draws.add (b.layout)
-            elif b.endState == 'x':
-                x_wins.add (b.layout)
-            elif b.endState == 'o':
-                o_wins.add (b.layout)
+            if parent.endState == 'd':
+                draws.add (layout)
+            elif parent.endState == 'x':
+                x_wins.add (layout)
+            elif parent.endState == 'o':
+                o_wins.add (layout)
             else:
-                nones.add (b.layout)
+                nones.add (layout)
             AllBoards[layout] = parent
 
 
 CreateAllBoards ('_________', BoardNode ('_________'))
-# k = list (AllBoards.keys ())
-# layout = k[100]
-# print (layout)
-# node = AllBoards[layout]
-# node.print_board()
-# print (len (node.children))
+x = 0
+o = 0
+d = 0
+no = 0
+for key in AllBoards.keys ():
+    n = AllBoards[key]
+    if n.endState == 'x':
+        x += 1
+    elif n.endState == 'o':
+        o += 1
+    elif n.endState == 'd':
+        d += 1
+    else:
+        no += 1
 
-a = len (x_wins)
-b = len (o_wins)
-c = len (draws)
-d = len (nones)
-print ("Total Boards ", len (AllBoards))
-print ("X Wins       ", a)
-print ("O Wins       ", b)
-print ("Draws        ", c)
-print ("Nones        ", d)
-print (a + b + c + d)
+print ("x: ", x)
+print ("o: ", o)
+print ("d: ", d)
+print ("n: ", no)
+print ("total: ", (x + o + d + no))
+
+
+# print ("Total Boards ", len (AllBoards))
+# print ("X Wins       ", len (x_wins))
+# print ("O Wins       ", len (o_wins))
+# print ("Draws        ", len (draws))
+# print ("Nones        ", len (nones))
